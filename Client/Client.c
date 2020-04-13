711124dda0045ba7deaa6a98c728aa77bd19d510
AlbertCerfeda <cerfeda.albert@gmail.com>
2020-04-13 17:38:59 +0200

    Bug fixes

--- Client/Client.c
+++ Client/Client.c
@@ -1,232 +1,315 @@
+#include <arpa/inet.h>
+#include <curses.h>
+#include <json-c/json.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <sys/select.h>
 #include <termios.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdbool.h>
-#include <pthread.h>
-#include <curses.h>
+#include <unistd.h>
+//#include <cJSON.h>
 
-#define PORT 4444
-#define MAX_USER_MESSAGE 150
-#define MAX_IDENTITY_PREFIX 50
 
-void append(char* s, char c);
+///SET THESE ACCORDINGLY///
+#define SERVER_PORT 4444
+#define SERVER_IP "78.47.63.215"
 
-/*struct cursor{
-   int x;
-   int y;
-}chatCursor,textboxCursor;*/
-WINDOW * chatWindow;
-WINDOW * textboxWindow;
-int bufferCursor_index=0;
+
+//do NOT touch these
+#define DISCONNECT_COMMAND ":disconnect"
+#define MAX_USER_MESSAGE 150   // Max lenght of the message a user can write
+#define MAX_IDENTITY_PREFIX 50 // Max lenght of the identifier ("xxx.xxx.xxx.xxx:xxxx (User) :")
+////////
+
+WINDOW *chatWindow;         // The messages will appear here
+WINDOW *textboxWindow;      // The message appears here as the user types
+int bufferCursor_index = 0; // Specifies where to write the message (eg. He|llo)
 struct winsize w;
+void adjustWindowSizes();
 
-int clientSocket;
+int clientSocket;              // The socket which comunicates with the server
+void disconnect(int clientSocket); // Disconnects from the server
 
+pthread_t serverThread; // The tread that listens for incoming messages and prints them onto 'WINDOW chatWindow'
 void *ServerThread_work(void *arguments);
-int pthread_create(pthread_t *thread, const pthread_attr_t *attr,void *(*start_routine) (void *), void *arg);
-pthread_t serverThread;
-int lastInput();
-char *substring(char *string, int position, int length);
-void insert_substring(char *a, char *b, int position);
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
 
-int main(){
-	initscr();
-	timeout(-1);
-	
-	start_color();
-	init_pair(1,COLOR_WHITE,COLOR_BLACK);
-	init_pair(2,COLOR_WHITE, COLOR_MAGENTA);
+void insert_substring(char*, char*, int); // Inserts a string into a certain position into another string
+char* substring(char*, int, int); 
+void remCharAtIndex(char *str, int index);
 
-	
+void append(char *s, char c);
 
+void sig_handler(int signo); // Catches SIGINT signal (CTRL+C)
 
+/*struct json_object *parsed_json;
+struct json_object *arr_userProfiles;
+char *getUsernameByIndex(int index);
+char *getPasswordByIndex(int index);
+void reloadConfig();
+bool authenticate(int usrIndex, char *inUsername, char *inPassword);*/
 
+int main(int argc, char *argv[]) {
+  /* Part that i started to work on initially (user profiles w/json) but then
+    realized I didn't have enough time reloadConfig(); int usrIndex = 0;
 
-	/////INITIALIZING GRAPHICS W/texboxWIndow and chatWindow////////
-    adjustWindowSizes();
+    /////////////////////////////////
+    // LOGGING SEQUENCE
+    printf("Username: %s\n", getUsernameByIndex(usrIndex));
 
-	wbkgd(stdscr, COLOR_PAIR(1));
-	wbkgd(chatWindow, COLOR_PAIR(1));
-	wbkgd(textboxWindow, COLOR_PAIR(2));
-	keypad(textboxWindow, TRUE);
+    printf("password: ");
+    char *inputPassword;
+    scanf("%s", inputPassword);
 
-
-	wrefresh(chatWindow);
-	wrefresh(textboxWindow);
-	wrefresh(stdscr);
-	/*chatCursor.x=0;
-    chatCursor.y=0;
-    textboxCursor.x=0;
-    textboxCursor.y=0;*/
-	
-	
-	int ret;
-	struct sockaddr_in serverAddr;
-	char buffer[MAX_USER_MESSAGE];
-	char message[MAX_USER_MESSAGE+MAX_IDENTITY_PREFIX];
-	
-	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(clientSocket < 0){
-		wprintw(chatWindow,"[-]Error in connection.\n\r");
-		wrefresh(chatWindow);
-		endwin();
-		exit(1);
-	}
-	wprintw(chatWindow,"[+] Client Socket is created.\n\r");
-	wrefresh(chatWindow);
-	
-	memset(&serverAddr, '\0', sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0){
-		wprintw(chatWindow,"[-] Error in connection.\n\r");
-		wrefresh(chatWindow);
-		endwin();
-		exit(1);
-	}
-	wprintw(chatWindow,"[+] Connected to Server.\n\r");
-	wrefresh(chatWindow);
-
-	if(pthread_create(&serverThread, NULL, &ServerThread_work, NULL)==0){
-		wprintw(chatWindow,"[+] Thread created succesfully\n\r______________________________\n\r");
-		wrefresh(chatWindow);
-	}
-	else{
-		wprintw(chatWindow,"[-] Error in creating thread\n\r");
-		wrefresh(chatWindow);
-	}
-
-	
-	memset(buffer,0, sizeof(buffer));
-
-	werase(textboxWindow);
-	while (1){
-		wmove(textboxWindow, 0, bufferCursor_index);
-		wrefresh(textboxWindow);
-		wrefresh(chatWindow);
-		/*fd_set fds;
-    	FD_ZERO(&fds);
-    	FD_SET(0, &fds);
-		select(1, &fds, NULL, NULL, NULL);
-		*/
-
-		int intTyped=wgetch(textboxWindow);
-		
-   		char charTyped=intTyped;
-
-		switch (intTyped){
-		case 10: //Enter
-			if(!strlen(buffer)==0){
-				send(clientSocket, buffer, strlen(buffer)+1, 0);
-				if(strcmp(buffer, ":exit")==true){
-					close(clientSocket);
-					wprintw(chatWindow,"[-] Disconnected from server.\n\r");
-					wrefresh(chatWindow);
-					endwin();
-					exit(1);
-				}
-				memset(buffer,0, sizeof(buffer));
-				bufferCursor_index=0;
-				werase(textboxWindow);
-				refresh();
-			}
-			break;
-		case 260: //Left arrow
-			if(bufferCursor_index != 0)
-				bufferCursor_index--;
-			break;
-		case 261: //Right arrow
-			if(bufferCursor_index < strlen(buffer))
-				bufferCursor_index++;
-			break;
-		case 263: //Backspace
-			if(bufferCursor_index != 0){
-				remCharAtIndex(&buffer,bufferCursor_index-1);
-				wclear(textboxWindow);
-				wprintw(textboxWindow, buffer);
-				bufferCursor_index--;
-			}
-			break;
-		case 330: //Delete
-			if(bufferCursor_index<strlen(buffer)){
-				remCharAtIndex(&buffer,bufferCursor_index);
-				wclear(textboxWindow);
-				wprintw(textboxWindow, buffer);
-			}
-		break;
-		case KEY_RESIZE:
-			//adjustWindowSizes();
-		break;
-		default:
-			if(strlen(buffer) < MAX_USER_MESSAGE){
-				insert_substring(&buffer,&charTyped,bufferCursor_index+1);
-				wmove(textboxWindow,0,bufferCursor_index++);
-				if (strlen(buffer)!= bufferCursor_index){
-					wclear(textboxWindow);
-					wprintw(textboxWindow,buffer);
-				}
-				else{
-					waddch(textboxWindow, charTyped);
-				    wrefresh(textboxWindow);
-				}
-				
-			}
-			break;
-		}	
-	}
-	endwin();
-	return 0;
-}
-void append(char* s, char c) {
-        int len = strlen(s);
-        s[len] = c;
-        s[len+1] = '\0';
-}
-int lastInput(){
-    int r;
-    unsigned char c;
-    if ((r = read(0, &c, sizeof(c))) < 0) {
-		return r;
+    if (!strcmp(inputPassword, getPasswordByIndex(usrIndex))) {
+      printf("PASSWORD CORRETTA\n");
     } else {
-		return c;
+      printf("PASSWORD SBAGLIATA");
     }
+
+    do {
+    } while (authenticate(usrIndex, getUsernameByIndex(usrIndex),
+    inputPassword));
+
+    /////////////////////////////////
+    {   // User authenticated
+      { // Connected to server
+      }
+    }*/
+  initscr();
+  //timeout(-1);
+  noecho();
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_WHITE, COLOR_MAGENTA);
+
+  // INITIALIZING GRAPHICS W/textboxWindow and chatWindow////////
+  adjustWindowSizes();
+  wbkgd(stdscr, COLOR_PAIR(1));
+  wbkgd(chatWindow, COLOR_PAIR(1));
+  wbkgd(textboxWindow, COLOR_PAIR(2));
+  keypad(textboxWindow, TRUE);
+
+  wrefresh(chatWindow);
+  wrefresh(textboxWindow);
+  wrefresh(stdscr);
+
+  // CREATING SOCKET AND CONNECTING TO SERVER
+  int ret;
+  struct sockaddr_in serverAddr;
+  char *buffer[MAX_USER_MESSAGE]; // Message buffer for client
+  char *message[MAX_IDENTITY_PREFIX + MAX_USER_MESSAGE]; // Message that gets sent/received (identity part + Usr message + )
+
+  clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (clientSocket < 0) {
+    wprintw(chatWindow, "[-]Error in connection.\n\r");
+    wrefresh(chatWindow);
+    endwin();
+    exit(1);
+  }
+  wprintw(chatWindow, "\r[+] Client Socket is created.\n\r");
+  wrefresh(chatWindow);
+
+  memset(&serverAddr, '\0', sizeof(serverAddr));
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(SERVER_PORT);
+  serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+  if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+    wprintw(chatWindow, "[-] Error in connection.\n\r");
+    wrefresh(chatWindow);
+    endwin();
+    exit(1);
+  }
+  wprintw(chatWindow, "[+] Connected to Server.\n\r");
+  wrefresh(chatWindow);
+
+  if (pthread_create(&serverThread, NULL, &ServerThread_work, NULL) == 0) {
+    wprintw( chatWindow, "[+] Thread created succesfully\n\r______________________________\n\r");
+    wrefresh(chatWindow);
+  } else {
+    wprintw(chatWindow, "[-] Error in creating thread\n\r");
+    wrefresh(chatWindow);
+  }
+
+  //memset(buffer, '/0', sizeof(buffer));
+
+  werase(textboxWindow);
+  signal(SIGINT, sig_handler);//If the user causes SIGINT (CTRL+C)
+  signal(SIGHUP, sig_handler);//If the user causes SIGINT (CTRL+C)
+  char charTypedVector[2];
+  charTypedVector[1] = '\0';
+  
+  while (1) {
+    wmove(textboxWindow, 0, bufferCursor_index);//Moves the cursor in the right spot
+    wrefresh(chatWindow);
+    wrefresh(textboxWindow);
+    
+    int intTyped = wgetch(textboxWindow); //Waits and then returns input
+    charTypedVector[0] = (char)intTyped; //Casts the input
+    
+    switch (intTyped) {
+
+    case 10: // Enter
+      if (!strlen(&buffer) == 0) { //If the message typed sin't null
+        /* For some reason this bugs everything out. I don't have time to fix it :(
+        if (strcmp(buffer, DISCONNECT_COMMAND) == true) {
+          disconnect(clientSocket);
+        } else {*/
+          send(clientSocket, buffer, strlen(buffer) + 1, 0);
+        //}
+
+        memset(buffer, 0, sizeof(buffer));
+        bufferCursor_index = 0; //Since the message is sent, the cursor goes back to the beginning
+        werase(textboxWindow);
+        refresh();
+      }
+      break;
+    case 258: //Down arrow
+
+    break;
+    case 259: //Up arrow
+
+    break;
+    case 260: // Left arrow = Shifts cursor to the left
+      if (bufferCursor_index != 0)
+        bufferCursor_index--;
+      break;
+    case 261: // Right arrow = Shifts cursor to the right
+      if (bufferCursor_index < strlen(buffer))
+        bufferCursor_index++;
+      break;
+    case 263: // Backspace
+      if (bufferCursor_index != 0) {
+        remCharAtIndex(&buffer, bufferCursor_index - 1);
+        wclear(textboxWindow);
+        wprintw(textboxWindow, buffer);
+        bufferCursor_index--;
+      }
+      break;
+    case 330: // Delete
+      if (bufferCursor_index < strlen(buffer)) {
+        remCharAtIndex(&buffer, bufferCursor_index);
+        wclear(textboxWindow);
+        wprintw(textboxWindow, buffer);
+      }
+      break;
+    case KEY_RESIZE: //When the window gets resized
+      adjustWindowSizes();
+      break;
+    default: //If a normal char gets typed
+      if (strlen(buffer) < MAX_USER_MESSAGE) {
+        //insert_substring(&buffer, &charTyped, bufferCursor_index+1);		// modifica di guarducci
+        insert_substring(&buffer, charTypedVector, bufferCursor_index+1);
+        wmove(textboxWindow, 0, bufferCursor_index++); wrefresh(textboxWindow);
+        if (strlen(buffer) != bufferCursor_index) {
+          wclear(textboxWindow); wrefresh(textboxWindow);
+          wprintw(textboxWindow, buffer); wrefresh(textboxWindow);
+        } else {
+          waddch(textboxWindow, charTypedVector[0]);wrefresh(textboxWindow);
+          wrefresh(textboxWindow);wrefresh(textboxWindow);
+        }
+      }
+      break;
+    }
+    wrefresh(textboxWindow);
+  }
+  endwin();
+  return 0;
 }
-void *ServerThread_work(void *arguments){
-	char message[200]; 
-	while (1){
-		wmove(textboxWindow, 0, bufferCursor_index);
-		wrefresh(textboxWindow);
-		memset(message,'\0', sizeof(message));
-		if(recv(clientSocket, message, 200, 0) < 0){
-			wprintw(chatWindow,"[-] Error in receiving data.\n\r");
-	 		wrefresh(chatWindow);
-		}else{				
-			wprintw(chatWindow,"%s\n\r", message);
-			wrefresh(chatWindow);
-		}
-	}
+void sig_handler(int signo) {
+  switch (signo){
+    case SIGINT:
+    case SIGHUP:
+    case SIGQUIT:
+      break;
+  }
+    disconnect(clientSocket);
+  endwin();
+  exit(1);
 }
 
-void insert_substring(char *a, char *b, int position){
+/* Part that i started to work on initially (user profiles w/json) but then
+realized I didn't have enough time void reloadConfig() 
+
+{ int *fp; char buffer[1024]; struct json_object *userProfile; size_t n_userProfiles;
+
+  fp = fopen("userProfiles.json", "r");
+
+  fread(&buffer, sizeof(buffer), 1, fp);
+  fclose(fp);
+  parsed_json = json_tokener_parse(buffer);
+  json_object_object_get_ex(parsed_json, "userProfiles", &arr_userProfiles);
+}
+bool authenticate(int usrIndex, char *inUsername, char *inPassword) {
+  if (!strcmp(inUsername, getUsernameByIndex(usrIndex)) &&
+      !strcmp(inPassword, getPasswordByIndex(usrIndex)))
+    return true;
+  else
+    return false;
+}
+
+char *getUsernameByIndex(int index) {
+  struct json_object *userProfile;
+  struct json_object *username;
+  userProfile = json_object_array_get_idx(arr_userProfiles, index);
+  json_object_object_get_ex(userProfile, "username", &username);
+  return json_object_get_string(username);
+}
+char *getPasswordByIndex(int index) {
+  struct json_object *userProfile;
+  struct json_object *password;
+  userProfile = json_object_array_get_idx(arr_userProfiles, index);
+  json_object_object_get_ex(userProfile, "password", &password);
+  return json_object_get_string(password);
+}*/
+
+void disconnect(int socket) { //Disconnects from the server
+  send(socket, DISCONNECT_COMMAND, strlen(DISCONNECT_COMMAND) + 1, 0);
+  wprintw(chatWindow, "\n[-] Disconnected from server.\n\r");
+  wrefresh(chatWindow);
+  pthread_cancel(serverThread);
+  close(clientSocket);
+  serverThread = NULL;
+  clientSocket = NULL;
+}
+void append(char *s, char c) {
+  int len = strlen(s);
+  s[len] = c;
+  s[len + 1] = '\0';
+}
+void *ServerThread_work(void *arguments) { //Continuosly listens for messages
+  char message[200];
+  while (1) {
+    wmove(textboxWindow, 0, bufferCursor_index);
+    wrefresh(textboxWindow);
+    memset(message, '\0', sizeof(message));
+    if (recv(clientSocket, message, 200, 0) < 0) {
+      wprintw(chatWindow, "[-] Error in receiving data.\n\r");
+      wrefresh(chatWindow);
+    } else {
+      wprintw(chatWindow, "%s\n\r", message);
+      wrefresh(chatWindow);
+    }
+  }
+}
+void insert_substring(char *a, char *b, int position)
+{
    char *f, *e;
    int length;
-   
+ 
    length = strlen(a);
-   
-   f = substring(a, 1, position - 1 );  
+ 
+   f = substring(a, 1, position - 1 );      
    e = substring(a, position, length-position+1);
-
+ 
    strcpy(a, "");
    strcat(a, f);
    free(f);
@@ -234,41 +317,41 @@ void insert_substring(char *a, char *b, int position){
    strcat(a, e);
    free(e);
 }
-char *substring(char *string, int position, int length){
+ 
+char *substring(char *string, int position, int length) 
+{
    char *pointer;
    int c;
  
    pointer = malloc(length+1);
-   
+ 
    if( pointer == NULL )
        exit(EXIT_FAILURE);
  
-   for( c = 0 ; c < length ; c++ )
-      *(pointer+c) = *((string+position-1)+c);      
-       
+   for( c = 0 ; c < length ; c++ ) 
+      *(pointer+c) = *((string+position-1)+c);       
+ 
    *(pointer+c) = '\0';
  
    return pointer;
 }
-void remCharAtIndex(char* str, int index){
-	if(str[index] != '\0'){
-		memmove(&str[index], &str[index + 1], strlen(str) - index);
-	}
+void remCharAtIndex(char *str, int index) {
+  if (str[index] != '\0') {
+    memmove(&str[index], &str[index + 1], strlen(str) - index);
+  }
 }
-void refreshTermSize(){
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-}
-void adjustWindowSizes(){
-	refreshTermSize();
-	wchar_t * text;
+void refreshTermSize() { ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); }
+void adjustWindowSizes() {
+  refreshTermSize();
+  wchar_t *text;
 
-	if(textboxWindow!=NULL)
-		winchstr(textboxWindow,&text);
-	textboxWindow = newwin(1, w.ws_col, w.ws_row-1, 0);
-	wprintw(textboxWindow, text);
+  if (textboxWindow != NULL)
+    winchstr(textboxWindow, &text);
+  textboxWindow = newwin(1, w.ws_col, w.ws_row - 1, 0);
+  wprintw(textboxWindow, text);
 
-	if(chatWindow!=NULL)
-		winchstr(chatWindow,&text);
-	chatWindow = newwin(w.ws_row-1, w.ws_col, 0, 0);
-	wprintw(chatWindow, text);
+  if (chatWindow != NULL)
+    winchstr(chatWindow, &text);
+  chatWindow = newwin(w.ws_row - 1, w.ws_col, 0, 0);
+  wprintw(chatWindow, text);
 }
--- Server/Server.c
+++ Server/Server.c
@@ -9,24 +9,41 @@
 #include <stdbool.h>
 #include <pthread.h>
 
-#define PORT 4444
-#define MAX_USERS_CONNECTED 10
+///SET THESE ACCORDINGLY///
+#define SERVER_PORT 4444
+#define SERVER_IP "78.47.63.215"
+
+
+//do NOT touch these
+#define DISCONNECT_COMMAND ":disconnect"
+#define MAX_USER_MESSAGE 150   // Max lenght of the message a user can write
+#define MAX_IDENTITY_PREFIX 50 // Max lenght of the identifier ("xxx.xxx.xxx.xxx:xxxx (User) :")
+////////
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr,void *(*start_routine) (void *), void *arg);
 
-struct ConnectedUser{
-    int socket;
+struct user{ //Connected users are saved into a linked list and removed when disconnected
+    int socket; //The socket comunicating with that user
 	struct sockaddr_in Addr;
-	pthread_t thread;
-    char name[10];
-} ConnUsers[MAX_USERS_CONNECTED];
-int ConnUsersCount=0;
+	pthread_t thread; //Thread handling that user
+	char name[10]; //Name of the user
+};
+struct node { //Linked list containing all the users
+	struct user *user;
+	int key;
+   	struct node *next;
+};
+   
+struct node *head; //First element of the list
 
-void MessBroadcast(char * message);
-int addUser(int newUserSocket, char newName[10], struct sockaddr_in newUserAddr);
-char * findName(int searchSocket);
+void MessBroadcast(char * message); //Sends a message to all the connected users
+int addUser(int newUserSocket, char newName[10], struct sockaddr_in newUserAddr); //Adds a newly connected user to the linked list, and creates a thread to listen to its messages
+char * findName(int searchSocket); //Returns the name of the player which has that specific socket
 
+int userListLength();
 int main(){
+	
+
     int sockfd, ret;
 	struct sockaddr_in serverAddr;
 
@@ -35,11 +52,7 @@ int main(){
 
 	socklen_t addr_size;
 
-	//char buffer[100];
-	//pid_t childpid;
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0){
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		printf("[-] Error in connection.\n");
 		exit(1);
 	}
@@ -47,71 +60,65 @@ int main(){
 
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverAddr.sin_port = htons(4444);
+	serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
 	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	if(ret < 0){
 		printf("[-] Error in binding.\n");
 		exit(1);
 	}
-	printf("[+] Bind to port %d\n", 4444);
 
 	if(listen(sockfd, 10) == 0){
-		printf("[+] Listening....\n");
+		printf("[+] Server Running:\n%s:%d\nListening....\n",SERVER_IP,SERVER_PORT);
 	}else{
 		printf("[-] Error in binding.\n");
 	}
 
-
-	while(1){
+	while(1){ //This loop continuosly accepts incoming requests
 		newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
 		
-		//////////////////////
-
 		char name[10]="";
-		sprintf(name,"Client%d", ConnUsersCount+1);
+		sprintf(name,"Client%d", userListLength()); //A name gets generated
 
-		if(addUser(newSocket,name,newAddr)==1){
+		if(addUser(newSocket,name,newAddr)==1){//The new user gets added to the linked list and a thread gets created to receive its messages
 			printf("Connection accepted from %s:%d  '%s' socket(%d)\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port), name, newSocket);
-			printf("ConnUsersCount== %d\n", ConnUsersCount);
-					
+			printf("ConnUsersCount== %d\n", userListLength());		
 		}
 	}
-	close(newSocket);
 	return 0;
 }
 
 void *perform_work(void *arguments){
-    
+	struct node *ptr = head;
+
+	while(ptr != NULL) { //The thread looks for himself in the linked list
+		if (ptr->user->thread != pthread_self()){
+			ptr = ptr->next;
+		}else{
+			break;
+		}
+   	}
 	
-	int USER_INDEX=0;
-
-	bool success=false;
-	for(USER_INDEX=0; ConnUsers[USER_INDEX].thread!=pthread_self() && USER_INDEX<ConnUsersCount;USER_INDEX++){
-
-	}
-	struct ConnectedUser *user= &ConnUsers[USER_INDEX];
+	struct user *user= ptr->user;
 
 	char * address = inet_ntoa(user->Addr.sin_addr);
 	uint16_t port = ntohs(user->Addr.sin_port);
 	char * name= user->name;
-
-	char buffer[100]; //Where the received message will be stored
-	char message[200]; //Where the final message that will be sent is stored
 	
-	while(1){
+	char buffer[MAX_USER_MESSAGE]; // Message buffer for client
+    char message[MAX_IDENTITY_PREFIX + MAX_USER_MESSAGE]; // Message that gets sent/received (identity part + Usr message + )
+	
+	while(1){ //Continuosly listens for incoming messages
 		recv(user->socket, buffer, 100, 0);
 		if(strcmp(buffer, "")!=0){
-			if(strcmp(buffer, ":exit") == 0){
-				printf("Disconnected from %s:%d\n", address, port);
+			if(strcmp(buffer, DISCONNECT_COMMAND) == 0){ //If the user asks for it, the server disconnects him
+				disconnect(user->socket);
 				break;
 			}
-			else{
-				printf("Message from [%s:%d (%s)] = '%s' - Brodcasting...\n", address,port, name,buffer);
-				
+			else{ //Otherwise broadcasts the message to the rest of th users
+				printf("Message from [%s:%d (%s)] = '%s' - Brodcasting...\n",address,port, name,buffer);
 				snprintf(message,sizeof(message),"%s:%d (%s) : %s", address,port, name,buffer);
-
 				MessBroadcast(message);
 			}
 		}
@@ -119,26 +126,77 @@ void *perform_work(void *arguments){
 		memset(message,0, sizeof(message));
 	}
 }
-
-void MessBroadcast(char * message){
-	for(int n=0;n<=ConnUsersCount-1;n++){
-		printf("Sending message to %s:%d '%s' socket(%d)\n", inet_ntoa(ConnUsers[n].Addr.sin_addr), ntohs(ConnUsers[n].Addr.sin_port), ConnUsers[n].name, ConnUsers[n].socket);
-		send(ConnUsers[n].socket, message, strlen(message)+1, 0);
+void disconnect(int socket){//Closes a specific user's socket and removes him from the linked list 
+	struct node *user = head;
+	while(user != NULL) {
+		if (user->user->socket == socket){
+			break;
+		}else{
+			user = user->next;
+		} 	
+   	}
+	if (user != NULL){
+		if(user == head){
+			head = head->next;
+		}
+		else{
+			struct node *prev= head;
+			while(prev->next != user){
+				prev = prev->next;
+			}
+			prev->next = user->next;
+			
+	   		
+		}
+		printf("Disconnected from %s:%d\n", inet_ntoa(user->user->Addr.sin_addr), ntohs(user->user->Addr.sin_port));
+		close(user->user->socket);
+		free(user);
+	}
+	else{
+		return;
 	}
 }
-int addUser(int newUserSocket, char newName[10], struct sockaddr_in newUserAddr){
-	if( ConnUsersCount < MAX_USERS_CONNECTED){
-		ConnUsers[ConnUsersCount].socket=newUserSocket;
+void MessBroadcast(char * message){//Sends a string to all the connected users
+	struct node *current = head;
+	while(current != NULL){
+		printf("Sending message to %s:%d '%s' socket(%d)\n", inet_ntoa(current->user->Addr.sin_addr), ntohs(current->user->Addr.sin_port), current->user->name, current->user->socket);
+		send(current->user->socket, message, strlen(message)+1, 0);
+
+		current = current->next;
+	}
+}
+int addUser(int newUserSocket, char newName[10], struct sockaddr_in newUserAddr){ //Adds a newly connected user to the linked list, and creates a thread which listens to its messages
+		int key=0;
+		struct node *newUser = head;
+		while(newUser != NULL) {//Goes in the back of the linked list
+      		newUser = newUser->next;
+			key++;
+   		}
+		newUser = (struct node*) malloc(sizeof(struct node));
+		newUser->key = key;
+		newUser->user = (struct user*) malloc(sizeof(struct user));
+		strncpy(newUser->user->name, newName,10);
+		newUser->user->socket=newUserSocket;
+		newUser->user->Addr=newUserAddr;
+		newUser->next=head;
+		head=newUser;
 		
-		ConnUsers[ConnUsersCount].Addr=newUserAddr;
-		strncpy(ConnUsers[ConnUsersCount].name,newName,10);
-		if(pthread_create(&ConnUsers[ConnUsersCount++].thread, NULL, perform_work, NULL)==0){
+		if(pthread_create(&newUser->user->thread, NULL, perform_work, NULL)==0){
 			printf("[+] Thread created succesfully\n");
+			return 1;
 		}
 		else{
 			printf("[-] Error in creating thread");
+			return 1;
 		}
-		return 1;
-	}
-	return 0;
+}
+int userListLength() {//Returns the lenght of the linked list
+   int length = 0;
+   struct node *current;
+	
+   for(current = head; current != NULL; current = current->next) {
+      length++;
+   }
+	
+   return length;
 }
